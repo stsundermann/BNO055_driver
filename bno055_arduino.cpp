5ec03b21ef310ce96a30e921cdce20b6bf40e703
Stephan Sundermann <stephansundermann@gmail.com>
2019-05-27 10:27:21 +0200

    Add arduino support

---
+++ bno055_arduino.cpp
@@ -0,0 +1,186 @@
+/*
+****************************************************************************
+* Copyright (C) 2015 - 2016 Bosch Sensortec GmbH
+*
+* bno055_support.c
+* Date: 2016/03/14
+* Revision: 1.0.4 $
+*
+* Usage: Sensor Driver support file for BNO055 sensor
+*
+****************************************************************************
+* License:
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*   Redistributions of source code must retain the above copyright
+*   notice, this list of conditions and the following disclaimer.
+*
+*   Redistributions in binary form must reproduce the above copyright
+*   notice, this list of conditions and the following disclaimer in the
+*   documentation and/or other materials provided with the distribution.
+*
+*   Neither the name of the copyright holder nor the names of the
+*   contributors may be used to endorse or promote products derived from
+*   this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER
+* OR CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+* OR CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+* ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+*
+* The information provided is believed to be accurate and reliable.
+* The copyright holder assumes no responsibility
+* for the consequences of use
+* of such information nor for any infringement of patents or
+* other rights of third parties which may result from its use.
+* No license is granted by implication or otherwise under any patent or
+* patent rights of the copyright holder.
+**************************************************************************/
+/*---------------------------------------------------------------------------*
+ Includes
+*---------------------------------------------------------------------------*/
+#include "bno055.h"
+#include "bno055_arduino.h"
+#include <Arduino.h>
+#include <Wire.h>
+
+/*--------------------------------------------------------------------------*
+*	The following API is used to map the I2C bus read, write, delay and
+*	device address with global structure bno055_t
+*-------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------*
+ *  By using bno055 the following structure parameter can be accessed
+ *	Bus write function pointer: BNO055_WR_FUNC_PTR
+ *	Bus read function pointer: BNO055_RD_FUNC_PTR
+ *	Delay function pointer: delay_msec
+ *	I2C address: dev_addr
+ *--------------------------------------------------------------------------*/
+s8 bno055_arduino_init(bno055_t* bno055)
+{
+	bno055->bus_write = BNO055_I2C_bus_write;
+	bno055->bus_read = BNO055_I2C_bus_read;
+	bno055->delay_msec = BNO055_delay_msek;
+	bno055->dev_addr = BNO055_I2C_ADDR1;
+
+	return bno055_init(bno055);
+}
+
+/************** I2C buffer length******/
+
+#define	I2C_BUFFER_LEN 8
+#define I2C0 5
+/*-------------------------------------------------------------------*
+*
+*	This is a sample code for read and write the data by using I2C
+*	Use either I2C  based on your need
+*	The device address defined in the bno055.h file
+*
+*--------------------------------------------------------------------*/
+
+/*	\Brief: The API is used as I2C bus write
+ *	\Return : Status of the I2C write
+ *	\param dev_addr : The device address of the sensor
+ *	\param reg_addr : Address of the first register,
+ *   will data is going to be written
+ *	\param reg_data : It is a value hold in the array,
+ *		will be used for write the value into the register
+ *	\param cnt : The no of byte of data to be write
+ */
+s8 BNO055_I2C_bus_write(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt)
+{
+	s32 BNO055_iERROR = BNO055_INIT_VALUE;
+	u8 array[I2C_BUFFER_LEN];
+	u8 stringpos = BNO055_INIT_VALUE;
+
+	array[BNO055_INIT_VALUE] = reg_addr;
+	for (stringpos = BNO055_INIT_VALUE; stringpos < cnt; stringpos++) {
+		array[stringpos + BNO055_I2C_BUS_WRITE_ARRAY_INDEX] = *(reg_data + stringpos);
+	}
+
+	Wire.beginTransmission(dev_addr);
+	for (uint8_t i = 0; i < cnt + 1; i++) {
+		Wire.write(array[i]);
+	}
+	Wire.endTransmission();
+	
+	/*
+	* Please take the below APIs as your reference for
+	* write the data using I2C communication
+	* "BNO055_iERROR = I2C_WRITE_STRING(DEV_ADDR, ARRAY, CNT+1)"
+	* add your I2C write APIs here
+	* BNO055_iERROR is an return value of I2C read API
+	* Please select your valid return value
+	* In the driver BNO055_SUCCESS defined as 0
+    * and FAILURE defined as -1
+	* Note :
+	* This is a full duplex operation,
+	* The first read data is discarded, for that extra write operation
+	* have to be initiated. For that cnt+1 operation done
+	* in the I2C write string function
+	* For more information please refer data sheet SPI communication:
+	*/
+	return (s8)BNO055_iERROR;
+}
+
+ /*	\Brief: The API is used as I2C bus read
+ *	\Return : Status of the I2C read
+ *	\param dev_addr : The device address of the sensor
+ *	\param reg_addr : Address of the first register,
+ *  will data is going to be read
+ *	\param reg_data : This data read from the sensor,
+ *   which is hold in an array
+ *	\param cnt : The no of byte of data to be read
+ */
+s8 BNO055_I2C_bus_read(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt)
+{
+	s32 BNO055_iERROR = BNO055_INIT_VALUE;
+	u8 array[I2C_BUFFER_LEN] = {BNO055_INIT_VALUE};
+	u8 stringpos = BNO055_INIT_VALUE;
+
+	array[BNO055_INIT_VALUE] = reg_addr;
+
+	/* Please take the below API as your reference
+	 * for read the data using I2C communication
+	 * add your I2C read API here.
+	 * "BNO055_iERROR = I2C_WRITE_READ_STRING(DEV_ADDR,
+	 * ARRAY, ARRAY, 1, CNT)"
+	 * BNO055_iERROR is an return value of SPI write API
+	 * Please select your valid return value
+     * In the driver BNO055_SUCCESS defined as 0
+     * and FAILURE defined as -1
+	 */
+	Wire.beginTransmission(dev_addr);
+	Wire.write(reg_addr);
+	Wire.endTransmission();
+
+	uint8_t i = 0;
+	Wire.requestFrom(dev_addr, cnt);
+	while (Wire.available()) {
+		reg_data[i] = Wire.read();
+		i++;
+	}
+
+	for (stringpos = BNO055_INIT_VALUE; stringpos < cnt; stringpos++)
+		*(reg_data + stringpos) = array[stringpos];
+	return (s8)BNO055_iERROR;
+}
+/*	Brief : The delay routine
+ *	\param : delay in ms
+*/
+void BNO055_delay_msek(u32 msek)
+{
+	delay(msek);
+}
